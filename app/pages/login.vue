4222f388b847de571bf8bd33e3f6d4382c30e337
parkseojin <juuck14@snbplus.co.kr>
2022-11-11 17:54:19 +0900

    dorf3

---
+++ app/pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="text"/> ID<br/>
+    <input type="text"/> PASSWORD<br/>
+    <button type="button">LOGIN</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default {
+    layout: 'client-layout'
+}
+</script>
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,14 @@
-console.log("dorf"); 
-console.log("dorf"); 
-console.log("dorf"); 
+const id = document.querySelector("#id"),
+password = document.querySelector("#password"),
+loginBtn = document.querySelector("button");
+
+const login = () => {
+    const req = {
+        id: id.value,
+        password: password.value
+    };
+    console.log(req)
+}
+
+loginBtn.addEventListener("click", login);
+
