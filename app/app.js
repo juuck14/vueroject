e48596209dc64623fa0da7268e8e2dd67d61279c
parkseojin <juuck14@snbplus.co.kr>
2022-11-15 18:11:45 +0900

    dorf11

--- app/app.js
+++ app/app.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require('dotenv');
-dotenv.config();
+const morgan = require('morgan')
+const accessLogStream = require('./src/config/log')
 
 const app = express();
+dotenv.config();
 
 const home = require("./src/routes/home")
+
+
 
 app.set("views", __dirname + "/src/views")
 app.set("view engine", "ejs")
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(morgan("common", { stream: accessLogStream }))
 
 
 app.use("/", home)
